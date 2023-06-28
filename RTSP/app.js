b95c15cbbc7e8ee01c9f4d4c228dd650768d644c
hskim <75778561+kingmuraa@users.noreply.github.com>
2023-06-28 09:05:55 +0900

    rstp

--- RTSP/app.js
+++ RTSP/app.js
@@ -3,7 +3,7 @@ const app = require('express')(),
   io = require('socket.io')(server),
   rtsp = require('rtsp-ffmpeg');
 server.listen(3580); //열고 싶은 포트번호 입력
-var uri = 'rtsp://admin:cosmotier1!@192.168.0.64:554/ISAPI/streaming/channels/101', // 하이크비전 
+var uri = 'rtsp://ID:PW@IP:Port/ISAPI/streaming/channels/101', // 하이크비전 
   stream = new rtsp.FFMpeg({input: uri});
 io.on('connection', function(socket) {
   var pipeStream = function(data) {
